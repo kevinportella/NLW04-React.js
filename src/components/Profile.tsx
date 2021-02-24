0850d236acb0b5e67c76c456b0462955f5c800b5
Kevin Portella <kevinportella@gmail.com>
2021-02-24 15:12:43 -0300

    Etapa 3

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -6,7 +6,7 @@ export function Profile() {
     <div className={styles.profileContainer}>
       <img src="https://github.com/kevinportella.png" alt="Kevinho Delas" />
       <div>
-        <strong>Kevinho Delas</strong>
+        <strong>Kevin Portella</strong>
         <p>
           <img src="icons/level.svg" alt="Level" />
           Lvl. 1</p>
